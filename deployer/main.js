8ce4ad435c93386eead1e9268411711cc52111c1
Yulong Wu <yulong@aion.network>
2018-10-23 14:53:25 +0800

    Send transaction one-by-one

--- deployer/main.js
+++ deployer/main.js
@@ -1,32 +1,33 @@
-// web3
-var Web3 = require('aion-web3')
+var Web3 = require('aion-web3');
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+var fs = require("fs");
 
-// readline
-var reader = require('readline').createInterface({
-  input: require('fs').createReadStream('../transactions.txt')
-});
+function handler(lines, current) {
+    console.log("Current raw tx: " + lines[current]);
 
-// parse the transactions and send them to the network
-var processed = 0;
-reader.on('line', function (line) {
-  web3.eth.sendRawTransaction(line, function(err, hash) {
-    if (err) {
-      console.error("Failed to send transaction: " + err);
-      process.exit(1);
-    }
-    var wait = 60000 + processed * 1000;
-    console.log("Tx hash: " + hash + ", wait for " + wait/1000 + " seconds");
+    web3.eth.sendRawTransaction(lines[current], function(err, hash) {
+        if (err) {
+            console.error("Failed to send transaction: " + err);
+            process.exit(1);
+        }
+        var wait = 10000;
+        console.log("Tx hash: " + hash + ", wait for " + wait / 1000 + " seconds");
 
-    setTimeout(() => {
-      var receipt = web3.eth.getTransactionReceipt(hash);
-      if (receipt) {
-        console.log(receipt);
-      } else {
-        console.error("Failed to retrieve the receipt for transaction: " + hash);
-      }
-    }, wait);
+        setTimeout(() => {
+            var receipt = web3.eth.getTransactionReceipt(hash);
+            if (receipt) {
+                console.log(receipt);
+                if (current < lines.length) {
+                    handler(lines, current + 1);
+                }
+            } else {
+                console.error("Failed to retrieve the receipt for transaction: " + hash);
+            }
+        }, wait);
+    });
+}
 
-    processed++;
-  });
+fs.readFile("../transactions.txt", "utf8", function(err, contents) {
+    var lines = contents.trim().split('\n');
+    handler(lines, 0);
 });
