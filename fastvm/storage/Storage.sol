1be3e295782bf47ae0d65ca5604f5e91eeb2c5de
Yulong Wu <yulong@aion.network>
2018-10-24 16:53:10 +0800

    Add storage reset test

--- fastvm/storage/Storage.sol
+++ fastvm/storage/Storage.sol
@@ -2,8 +2,13 @@ pragma solidity ^0.4.0;
 
 contract Test {
 
-    uint result2;
+    uint result2 = 3;
+
     function f() {
         result2 = result2 + 5;
+    }
+
+    function reset() {
+        result2 = 0;
     }
 }
